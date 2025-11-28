c2d6eea3b7738aef132786976e26d9b5f697f1ae
Moritz Sackmann <moritz.sackmann@gmail.com>
2025-11-28 15:54:12 +0100

    Fix type hints

--- src/strange_attractors/utils/ringbuffer.py
+++ src/strange_attractors/utils/ringbuffer.py
@@ -2,13 +2,13 @@ import numpy as np
 
 
 class TrajectoryBuffer:
-    def __init__(self, shape: tuple[int], ring_axis=1):
+    def __init__(self, shape: tuple[int, ...], ring_axis=1):
         self._trajectory = np.zeros(shape)
         self._ring_axis = ring_axis
         assert ring_axis == 1, "Other ring axes than 1 not supported"
         self._buffer_size = shape[ring_axis]
 
-    def append(self, data: np.array):
+    def append(self, data: np.ndarray):
         num_items = data.shape[self._ring_axis]
         # move back part to front
         remaining = self._buffer_size - num_items
@@ -16,5 +16,5 @@ class TrajectoryBuffer:
         # Insert new back part
         self._trajectory[:, remaining:] = data
 
-    def get(self) -> np.array:
+    def get(self) -> np.ndarray:
         return self._trajectory
